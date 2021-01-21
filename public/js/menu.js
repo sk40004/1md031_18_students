c744ef314559fe4b0da1e91b6eba7a1c56ed8842
Sana <sanazk2008@gmail.com>
2021-01-21 20:44:09 +0100

    added sample burgers and menu with css

---
+++ public/js/menu.js
@@ -0,0 +1,32 @@
+var food = [
+  
+    {
+    "name": "The Fire Burger",
+    "kCal": 750,
+    "lactose": true,
+    "gluten": true,
+    "img": "../img/fryburger.jpeg"
+    ,"stock":5
+    },
+    {
+        "name": "Fried Turkey Burger",
+        "kCal": 600,
+        "lactose": true,
+        "gluten": false,
+        "img": "../img/burger.jpg"
+        ,"stock":5
+    },
+
+    {
+        "name": "A double Cheese Burger",
+        "kCal": 1800,
+        "lactose": true,
+        "gluten": true,
+        "img": "../img/double_burger.jpg"
+        ,"stock":5
+    }
+
+  
+
+]
+
